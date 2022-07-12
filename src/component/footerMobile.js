fda96b0512f0d24e08ed76edb18e527fe19a831b
petuniahtf <priscilliaweber6@hotmail.com>
2022-07-13 00:36:58 +0200

    footerMobile

--- src/component/footerMobile.js
+++ src/component/footerMobile.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { IconButton } from "@chakra-ui/react";
+import {  BsListUl,  BsFillBookmarkFill, BsFillPersonFill} from 'react-icons/bs'
+import { IoHome} from 'react-icons/io5'
+import {  FiPlusCircle} from 'react-icons/fi'
 
-const FooterMobile = () =>{
-    return(
-        <>
-        </>
+const FooterMobile = () => {
+
+    const nameIcon = [<IoHome/>, <BsListUl/>, <FiPlusCircle/>, <BsFillBookmarkFill/>, <BsFillPersonFill/> ]
+    return (
+
+        <div>
+           { nameIcon.map((nom)=> <IconButton
+                variant='ghost'
+                colorScheme={'green.400'}
+                aria-label='Call Sage'
+                fontSize='40px'
+                icon={nom}
+            />
+            )}
+        </div>
     )
 }
 
