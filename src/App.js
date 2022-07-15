c78396af876c7e35b60948c37b8a5c44eb8a5420
David Belleau <david-belleau@hotmail.com>
2022-07-15 09:56:50 +0200

    create 2 components + balises p

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import NutriNavDesktop from './components/NutriNavDesktop/NutriNavDesktop';
+import NutriNavMobile from './components/NutriNavMobile/NutriNavMobile';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <NutriNavDesktop />
+      <div style={{ color: 'white' }}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
+          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
+          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
+          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
+          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
+          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
+          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
+          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
+          erat. Cras ac mi turpis.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <p>
+          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
+          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
+          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
+          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
+          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
+          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
+          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
+          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
+          erat. Cras ac mi turpis.
+        </p>
+        <p>
+          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
+          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
+          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
+          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
+          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
+          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
+          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
+          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
+          erat. Cras ac mi turpis.
+        </p>
+        <p>
+          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
+          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
+          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
+          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
+          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
+          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
+          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
+          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
+          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
+          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
+          erat. Cras ac mi turpis.
+        </p>
+      </div>
+      <NutriNavMobile />
+    </>
+  )
 }
 
 export default App;
