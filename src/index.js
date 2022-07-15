95407a4c78ac786d39e300cc27513687df09f1e3
David Belleau <david-belleau@hotmail.com>
2022-07-15 09:57:41 +0200

    create theme chakra ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "gray.800"
+      }
+    }
+  },
+  colors: {
+    green: {
+      700: "#276749",
+      400: "#48BB78",
+      50: "#F0FFF4"
+    },
+    gray: {
+      800: "#1A202C",
+      400: "#A0AEC0"
+    }
+  },
+  fonts: {
+    body: "Poppins, sans-serif",
+  },
+  /* fontWeights: {
+    extraLight: 200,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+  } */
+})
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
