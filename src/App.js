61e4104445d7fa8df6b68eb9ee2afd8f9934cad6
David Belleau <david-belleau@hotmail.com>
2022-07-17 23:53:48 +0200

    delete balises p

--- src/App.js
+++ src/App.js
@@ -13,17 +13,17 @@ const theme = extendTheme({
       }
     }
   },
- /*  colors: {
-    green: {
-      700: "#276749",
-      400: "#48BB78",
-      50: "#F0FFF4"
-    },
-    gray: {
-      800: "#1A202C",
-      400: "#A0AEC0"
-    }
-  }, */
+  /*  colors: {
+     green: {
+       700: "#276749",
+       400: "#48BB78",
+       50: "#F0FFF4"
+     },
+     gray: {
+       800: "#1A202C",
+       400: "#A0AEC0"
+     }
+   }, */
   fonts: {
     body: "Poppins, sans-serif",
   },
@@ -40,64 +40,8 @@ const theme = extendTheme({
 function App() {
   return (
     <ChakraProvider theme={theme}>
-     {/*  <div className="App">
-        <Card/>
-      </div> */}
       <NutriNavDesktop />
-      <div style={{ color: 'white' }}>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-      </div>
+      <Card />
       <NutriNavMobile />
     </ChakraProvider>
   );
