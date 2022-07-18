a1dbc78d078a1dfb528444b79d21106c2c924beb
Mathieu <mathieu.herbelet@hotmail.fr>
2022-07-18 08:29:13 +0200

    fix/ Suppression des balises p + ajout de la div App et suppression du composant Card

--- src/App.js
+++ src/App.js
@@ -40,65 +40,10 @@ const theme = extendTheme({
 function App() {
   return (
     <ChakraProvider theme={theme}>
-     {/*  <div className="App">
-        <Card/>
-      </div> */}
-      <NutriNavDesktop />
-      <div style={{ color: 'white' }}>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-        <p>
-          Mauris sed magna tempor, pellentesque odio non, facilisis ligula.
-          Nunc porta iaculis sem ut pulvinar. Maecenas lobortis ornare ligula,
-          id fermentum elit. Proin ac malesuada lectus. Vestibulum ut nunc sed
-          leo suscipit faucibus. In hac habitasse platea dictumst. Morbi rutrum
-          ex in mi ullamcorper, non dignissim odio blandit. Nam rutrum pulvinar aliquam.
-          Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus
-          mus. Integer blandit a justo eu lobortis. Vestibulum dignissim nisi dui, at
-          tincidunt felis scelerisque vitae. Pellentesque iaculis augue turpis, vitae
-          eleifend elit efficitur a. Fusce quis dignissim magna. Praesent ullamcorper at
-          felis a sagittis. Phasellus justo risus, tincidunt et tortor eu, vulputate fermentum
-          erat. Cras ac mi turpis.
-        </p>
-      </div>
-      <NutriNavMobile />
+     <div className="App">
+         <NutriNavDesktop />
+         <NutriNavMobile />
+     </div>
     </ChakraProvider>
   );
 }
