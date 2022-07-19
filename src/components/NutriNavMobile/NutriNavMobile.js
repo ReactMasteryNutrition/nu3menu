7ee050f88c24e094d43418624bb1b2ebb7efc7a8
David Belleau <david-belleau@hotmail.com>
2022-07-19 23:32:38 +0200

    modify style + add link to + create variables

--- src/components/NutriNavMobile/NutriNavMobile.js
+++ src/components/NutriNavMobile/NutriNavMobile.js
@@ -1,56 +1,47 @@
 import './NutriNavMobile.css'
+import NutriRegister from '../../pages/Connection/NutriRegister'
 import {
     useDisclosure,
-    Button,
     Drawer,
     DrawerBody,
     DrawerOverlay,
     DrawerContent,
     DrawerCloseButton,
     Avatar,
-    Box
+    useMediaQuery
 } from '@chakra-ui/react'
 import { ChatIcon, BellIcon, QuestionIcon } from '@chakra-ui/icons'
+import { Link } from 'react-router-dom'
+import NutriLogin from '../../pages/Connection/NutriLogin'
 
 // const userToken = 'nu3menuToken'
 
 const DrawerVisitorMobile = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
+
     return (
         <>
             <button onClick={onOpen}>
                 <img src="./images/avatar_visitor.svg" alt="Logo du site" />
             </button>
-            <Drawer placement="bottom" isOpen={isOpen} onClose={onClose} >
+            <Drawer placement="bottom" isOpen={isOpen} onClose={onClose} size='full'>
                 <DrawerOverlay bg="rgba(160, 174, 192, 0.5)" />
-                <DrawerContent
-                    bg="#f0fff4"
-                    borderTopLeftRadius="2rem"
-                    borderTopRightRadius="2rem"
-                >
+                <DrawerContent bg="#f0fff4">
                     <DrawerCloseButton />
 
                     <div className='drawer__seperator--top'></div>
                     <DrawerBody
                         display="flex"
                         flexDirection="column"
-                        textAlign="center"
-                        marginTop="2rem"
+                        gap="1rem"
+                        position="relative"
+                        top="5rem"
+                        margin="0 auto"
+                        fontWeight="bold"
+                        fontSize="1.2rem"
                     >
-                        <div>
-                            <Button bg="#f0fff4" _hover={{ bg: "#f0fff4" }} >
-                                <img src='./images/login.svg'
-                                    alt='Une porte et une flèche' width="30rem" />
-                                Se connecter
-                            </Button>
-                        </div>
-                        <div>
-                            <Button bg="#f0fff4" _hover={{ bg: "#f0fff4" }}>
-                                <img src='./images/register.svg'
-                                    alt='Des avatars' width="30rem" />
-                                S'inscrire
-                            </Button>
-                        </div>
+                        <NutriLogin />
+                        <NutriRegister />
                     </DrawerBody>
                 </DrawerContent>
             </Drawer>
@@ -63,97 +54,92 @@ const DrawerUserMobile = () => {
 
     return (
         <>
-            <Box>
-                <button onClick={onOpen}>
-                    <Avatar size="sm" />
-                </button>
-            </Box>
-            <Drawer placement="bottom" isOpen={isOpen} onClose={onClose} bottom="4.625rem"
-            >
+            <button onClick={onOpen}>
+                <Avatar size="sm" />
+            </button>
+            <Drawer placement="bottom" isOpen={isOpen} onClose={onClose} size="full">
                 <DrawerOverlay bg="rgba(160, 174, 192, 0.5)" />
-                <DrawerContent
-                    bg="#f0fff4"
-                    borderTopLeftRadius="2rem"
-                    borderTopRightRadius="2rem"
-                >
+                <DrawerContent bg="#f0fff4">
                     <DrawerCloseButton />
 
                     <div className='drawer__seperator--top'></div>
                     <DrawerBody
                         display="flex"
                         flexDirection="column"
-                        justifyContent="center"
-                        textAlign="center"
-                        gap="1rem"
-                        marginTop="2rem"
-                        fontWeight="bold" >
-                        <a href='/' className='drawer_img--position'>
-                            <img src='./images/avatar_account.svg' alt='Avatar' />
+                        gap="2rem"
+                        position="relative"
+                        top="5rem"
+                        margin="0 auto"
+                        fontWeight="bold"
+                        fontSize="1.2rem">
+                        <Link to='/user' className='drawer_img--position'>
+                            <img src='./images/avatar_account.svg' alt='Avatar' style={{ marginRight: '0.5rem' }} />
                             <p>John</p>
-                        </a>
-                        <a href='/account' className='drawer_img--position'>
-                            <img src='./images/gear_wheel.svg' alt='Avatar' />
+                        </Link>
+                        <Link to='/user/account' className='drawer_img--position'>
+                            <img src='./images/gear_wheel.svg' alt='Avatar' style={{ marginRight: '0.5rem' }} />
                             <p >Mon compte</p>
-                        </a>
-                        <a href='/allmenus' className='drawer_img--position'>
-                            <img src='./images/card_menu.svg' alt='Carte de menus' />
+                        </Link>
+                        <Link to='/user/allmenus' className='drawer_img--position'>
+                            <img src='./images/card_menu.svg' alt='Carte de menus' style={{ marginRight: '0.5rem' }} />
                             <p>Tous mes menus</p>
-                        </a>
-                        <div>
-                            <a href='/alladvices'>
-                                <ChatIcon color="#48BB78" /> Tous mes avis
-                            </a>
-                        </div>
-                        <div>
-                            <a href='/notifications'>
-                                <BellIcon color="#48BB78" /> Notifications
-                            </a>
-                        </div>
-                        <div>
-                            <a href='/helpandsupport'>
-                                <QuestionIcon color="#48BB78" /> Aide et support
-                            </a>
-                        </div>
-                        <a href='/logout' className='drawer_img--position'>
-                            <img src="./images/logout.svg" alt='Une porte et une flèche' />
+                        </Link>
+                        <Link to='/user/alladvices'>
+                            <ChatIcon color="#48BB78" marginRight='0.5rem' /> Tous mes avis
+                        </Link>
+                        <Link to='/user/notifications'>
+                            <BellIcon color="#48BB78" marginRight='0.5rem' /> Notifications
+                        </Link>
+                        <Link to='/user/helpandsupport'>
+                            <QuestionIcon color="#48BB78" marginRight='0.5rem' /> Aide et support
+                        </Link>
+                        <Link to='/' className='drawer_img--position'>
+                            <img 
+                            src="./images/logout.svg" 
+                            alt='Une porte et une flèche' 
+                            style={{ marginRight: '0.5rem' }} />
                             <p>Se déconnecter</p>
-                        </a>
+                        </Link>
                     </DrawerBody>
                 </DrawerContent>
-            </Drawer >
+            </Drawer>
         </>
     )
 }
 
 const NavVisitorMobile = () => {
+    const [minWidth501] = useMediaQuery('(min-width: 501px)')
+
     return (
         <>
             <div id='navVisitorMobile'>
                 <div className="nav__separator--top"></div>
-                <DrawerVisitorMobile />
+                {minWidth501 ? null : <DrawerVisitorMobile />}
             </div>
         </>
     )
 }
 
 const NavUserMobile = () => {
+    const [minWidth501] = useMediaQuery('(min-width: 501px)')
+
     return (
         <>
             <div id='navUserMobile'>
                 <div className="nav__separator--top"></div>
-                <a href="/">
+                <Link to="/user">
                     <img src="./images/home.svg" alt="Maison" />
-                </a>
-                <a href="/menu">
+                </Link>
+                <Link to="/user/menu">
                     <img src="./images/menu.svg" alt="Menu" />
-                </a>
-                <a href="/create">
+                </Link>
+                <Link to="/user/createmenu">
                     <img src="./images/plus.svg" alt="Le signe plus" />
-                </a>
-                <a href="/bookmark">
+                </Link>
+                <Link to="/user/bookmark">
                     <img src="./images/bookmark.svg" alt="Marque-page" />
-                </a>
-                <DrawerUserMobile />
+                </Link>
+                {minWidth501 ? null : <DrawerUserMobile />}
             </div>
         </>
     )
@@ -169,4 +155,4 @@ const NutriNavMobile = () => {
     )
 }
 
-export default NutriNavMobile
+export default NutriNavMobile;
