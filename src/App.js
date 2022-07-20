f88692218ba9cda4738137fcad968819b9715119
Weber <71732780+petuniahtf@users.noreply.github.com>
2022-07-21 00:00:05 +0200

    Update App.js
    deletion of duplicates
     import { extendTheme, ChakraProvider } from '@chakra-ui/react'
    import UserApp from './userApp.js';

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import './App.css';
 import * as React from 'react';
 import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 import UserApp from './userApp';
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
-
-import UserApp from './userApp.js';
 
 
 
