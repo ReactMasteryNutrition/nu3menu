ff8be8a1b6017b1c66a258c02dcf3f5276f2e164
Mathieu <mathieu.herbelet@hotmail.fr>
2022-07-21 10:18:37 +0200

    fix/correction du nommage de la fonction 'ModalDesktopRegister' + modifs nommage var du process + supression de la méthode HandleChange + suppression du type Email

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,12 +7,12 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apikey: process.env.REACT_APP_FIREBASE_API_KEY,
+    apikey: process.env.REACT_APP_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
+    appId: process.env.REACT_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
