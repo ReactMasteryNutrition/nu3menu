d8d43af190465240e0e86a7a10989a73a51c822a
David Belleau <david-belleau@hotmail.com>
2022-07-25 01:11:32 +0200

    delete comment and import

--- src/App.js
+++ src/App.js
@@ -2,11 +2,6 @@ import './App.css';
 import * as React from 'react';
 import { extendTheme, ChakraProvider } from '@chakra-ui/react';
 import UserApp from './userApp';
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
-
-import UserApp from './userApp.js';
-
-
 
 const theme = extendTheme({
   styles: {
@@ -30,21 +25,13 @@ const theme = extendTheme({
   fonts: {
     body: "Poppins, sans-serif",
   },
-  /* fontWeights: {
-    extraLight: 200,
-    light: 300,
-    regular: 400,
-    medium: 500,
-    semibold: 600,
-    bold: 700,
-  } */
 })
 
 function App() {
   return (
-      <ChakraProvider theme={theme}>
-        <UserApp />
-      </ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <UserApp />
+    </ChakraProvider>
   );
 }
 
