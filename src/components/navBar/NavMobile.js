26a01f8b890ae7860b0d5704dedd5b083c7097e4
David Belleau <david-belleau@hotmail.com>
2022-07-28 20:00:28 +0200

    modify routes + add props

--- src/components/navBar/NavMobile.js
+++ src/components/navBar/NavMobile.js
@@ -1,4 +1,6 @@
-import { DrawerVisitor, DrawerUser } from './Drawer'
+import { /* DrawerVisitor, */ DrawerUser } from './Drawer'
+import { ModalRegister, ModalLogin } from "../../components/connection/Modal"
+
 import { ResponsiveWidth } from "../../utils/helper"
 import { Link } from 'react-router-dom'
 import { Flex, Box } from '@chakra-ui/react'
@@ -10,79 +12,83 @@ import { AiOutlineMenuUnfold } from 'react-icons/ai'
 
 const NavVisitor = () => {
     return (
-        <>
+        <Box
+            display={ResponsiveWidth() ? "none" : "block"}
+            position="fixed"
+            z-index="1"
+            bottom="0"
+            width="100vw"
+            height="4.5rem"
+            bg="#1a202c"
+        >
             <Box
-                display={ResponsiveWidth() ? "none" : "block"}
-                position="fixed"
+                position="absolute"
                 z-index="1"
-                bottom="0"
-                padding="1rem"
-                width="100vw"
-                height="4.5rem"
-                bg="#1a202c"
-            >
-                <Box
+                left="50%"
+                top="0"
+                height="0.3rem"
+                width="100%"
+                transform="translate(-50%, -50%)"
+                bg="#48bb78"
+            ></Box>
+            {ResponsiveWidth() ? null : (<>
+                <Flex
+                    gap="4rem"
                     position="absolute"
-                    z-index="1"
+                    top="50%"
                     left="50%"
-                    top="0"
-                    height="0.3rem"
-                    width="100%"
-                    transform="translate(-50%, -50%)"
-                    bg="#48bb78"
-                ></Box>
-                {ResponsiveWidth() ? null : <DrawerVisitor />}
-            </Box>
-        </>
+                    transform="translate(-50%, -50%)">
+                    <ModalLogin />
+                    <ModalRegister />
+                </Flex>
+            </>)}
+            {/* <DrawerVisitor/> */}
+        </Box>
     )
 }
 
 const NavUser = () => {
     return (
-        <>
-            <Flex
-                display={ResponsiveWidth() ? "none" : "flex"}
-                flexDirection="row"
-                justifyContent="center"
-                alignItems="center"
-                gap="2.5rem"
-                position="fixed"
-                zIndex="1"
-                bottom="0"
-                padding="1rem"
-                width="100vw"
-                height="4.5rem"
-                bg="#1a202c"
-            >
-                <Box
-                    position="absolute"
-                    z-index="1"
-                    left="50%"
-                    top="0"
-                    height="0.3rem"
-                    width="100%"
-                    transform="translate(-50%, -50%)"
-                    bg="#48bb78"
-                ></Box>
-                <Box>
-                    <Flex gap="2.2rem" alignItems="center">
-                        <Link to="/user" >
-                            <FaHome size="33" color="#48BB78" />
-                        </Link>
-                        <Link to="/user/menu">
-                            <AiOutlineMenuUnfold size="33" color="#48BB78" />
-                        </Link>
-                        <Link to="/user/createmenu">
-                            <BsPlusCircle size="33" color="#48BB78" />
-                        </Link>
-                        <Link to="/user/bookmark">
-                            <BsBookmarkFill size="33" color="#48BB78" />
-                        </Link>
-                        {ResponsiveWidth() ? null : <DrawerUser />}
-                    </Flex>
-                </Box>
-            </Flex >
-        </>
+        <Flex
+            display={ResponsiveWidth() ? "none" : "flex"}
+            flexDirection="row"
+            justifyContent="center"
+            alignItems="center"
+            position="fixed"
+            zIndex="1"
+            bottom="0"
+            width="100vw"
+            height="4.5rem"
+            bg="#1a202c"
+        >
+            <Box
+                position="absolute"
+                z-index="1"
+                left="50%"
+                top="0"
+                height="0.3rem"
+                width="100%"
+                transform="translate(-50%, -50%)"
+                bg="#48bb78"
+            ></Box>
+            <Box>
+                <Flex gap="2.2rem" alignItems="center">
+                    <Link to="/" >
+                        <FaHome size="33" color="#48BB78" />
+                    </Link>
+                    <Link to="/menu">
+                        <AiOutlineMenuUnfold size="33" color="#48BB78" />
+                    </Link>
+                    <Link to="/createmenu">
+                        <BsPlusCircle size="33" color="#48BB78" />
+                    </Link>
+                    <Link to="/bookmark">
+                        <BsBookmarkFill size="33" color="#48BB78" />
+                    </Link>
+                    {ResponsiveWidth() ? null : <DrawerUser />}
+                </Flex>
+            </Box>
+        </Flex >
     )
 }
 
@@ -90,8 +96,8 @@ const NavMobile = () => {
     return (
         <>
             {/* {userToken ? <NavUser/> : <NavVisitor/>} */}
-            {/* <NavVisitor /> */}
-            <NavUser />
+            <NavVisitor />
+            {/* <NavUser /> */}
         </>
     )
 }
