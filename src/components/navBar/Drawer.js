3a0a1347ce32bbfa4ba639dd03c3d42b355c871f
David Belleau <david-belleau@hotmail.com>
2022-07-30 17:30:31 +0200

    delete comments

--- src/components/navBar/Drawer.js
+++ src/components/navBar/Drawer.js
@@ -16,62 +16,6 @@ import { CgProfile } from 'react-icons/cg'
 import { IoMdSettings } from "react-icons/io"
 import { IoLogOut } from "react-icons/io5"
 import { MdOutlineMenuBook } from "react-icons/md"
-// import { FaUserAlt } from "react-icons/fa"
-/* 
-const DrawerVisitor = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-
-    return (
-        <>
-            <Button
-                bg="#1A202C"
-                _hover={{ bg: "#1A202C" }}
-                _active={{ bg: "#1A202C" }}
-                position="absolute"
-                top="50%"
-                left="50%"
-                transform="translate(-50%, -50%)"
-                onClick={onOpen}
-            >
-                <FaUserAlt
-                    color="#48BB78"
-                    // _hover={{filter: "brightness(0) saturate(100%) invert(94%) sepia(13%) saturate(319%) hueRotate(69deg) brightness(106%) contrast(101%)"}}
-                    size="50"
-                />
-            </Button>
-            <Drawer placement="bottom" isOpen={isOpen} onClose={onClose} >
-                <DrawerOverlay bg="rgba(160, 174, 192, 0.5)" />
-                <DrawerContent bg="#f0fff4">
-                    <DrawerCloseButton />
-                    <Box
-                        position="absolute"
-                        zIndex="1"
-                        left="50%"
-                        top="1.5rem"
-                        height="0.3rem"
-                        width="33.33%"
-                        transform="translate(-50%, -50%)"
-                        bg="#48bb78"
-                        borderRadius="0.3rem"
-                    ></Box>
-                    <DrawerBody
-                        display="flex"
-                        flexDirection="column"
-                        gap="1rem"
-                        position="relative"
-                        top="5rem"
-                        margin="0 auto"
-                        fontWeight="bold"
-                        fontSize="1.2rem"
-                    >
-                        <ModalLogin />
-                        <ModalRegister />
-                    </DrawerBody>
-                </DrawerContent>
-            </Drawer>
-        </>
-    )
-} */
 
 const DrawerUser = () => {
     const { isOpen, onOpen, onClose } = useDisclosure()
@@ -154,4 +98,4 @@ const DrawerUser = () => {
     )
 }
 
-export {/*  DrawerVisitor, */ DrawerUser }
+export { DrawerUser }
