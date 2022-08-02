704d6b68f2994cf557089d7eac6ca18b47d89ef1
petuniahtf <priscilliaweber6@hotmail.com>
2022-08-03 00:15:35 +0200

    localstorage delete add

--- src/components/meal/dayMeal.js
+++ src/components/meal/dayMeal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Heading, Image, Text} from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Image, Text} from "@chakra-ui/react";
 //import { weekMenu } from "../data/weekMenu";
 
 
@@ -9,18 +9,27 @@ const DayMeal = ({mealDayy,category}) =>{
 //console.log(`ici les weekmenu ${weekMenu[2].day}`)
 let week =  localStorage.getItem('week')
 let weekParse = JSON.parse(week)
+const keyWeek = Object.keys(weekParse)
+
+const handleClickDelete = () =>{
+    localStorage.removeItem('week');
+}
+
+console.log(`${keyWeek} voila les clef`)
 console.log(`teste recupération week  ${week}`)
 console.log(`${weekParse} on est parser`)
     return(
         <Box  w='100%' p={4} color={"green.400"}>
             <Heading as='h1' size='xl'> Tout mes menus</Heading>
             <Flex direction={'column'}>
-            {weekParse.map((dat)=> <Heading as='h3' size='xl'> {dat.day}</Heading>)}
+            {keyWeek.map((dat)=> <Heading key={dat.i} as='h3' size='xl'> {dat}</Heading>)}
             <Text>{category}</Text>
             <Image src="../data/fraise.jpg" alt="rimage" />
             <Text>{week}</Text>
-
-            </Flex>   
+            <Flex alignItems={'center'}>
+            <Button w={'20%'}  onClick ={handleClickDelete}>Supprimer</Button>
+            </Flex>
+            </Flex>  
        </Box>
 
     )
