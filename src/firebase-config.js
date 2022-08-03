003da4ae0d6294fbd6a3c1dab06f6dfa4a30bcc5
Mathieu <mathieu.herbelet@hotmail.fr>
2022-08-03 21:00:14 +0200

    feat/ Remplacement des informations de l'API en clair par variables env

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAnWbOx8gYsX0sYd7WodDJj06G4Ijr0kvE",
-    authDomain: "nu3menu.firebaseapp.com",
-    databaseURL: "https://nu3menu-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "nu3menu",
-    storageBucket: "nu3menu.appspot.com",
-    messagingSenderId: "1004160040248",
-    appId: "1:1004160040248:web:fcb4726cd9f80b0c7755cd",
-    measurementId: "G-D7SFH1YJQJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
