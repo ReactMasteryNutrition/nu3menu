206fc220fcbae12920621bdfbf98ca531618773c
David Belleau <david-belleau@hotmail.com>
2022-08-10 18:20:13 +0200

    style/modify import

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import AuthContextProvider from "./components/context/authContext";
+import AuthContextProvider from "./context/authContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
