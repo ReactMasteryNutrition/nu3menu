b22262661ddee0aab25efd134f7219abbbd73d8a
David Belleau <david-belleau@hotmail.com>
2022-08-11 19:17:31 +0200

    chore/add doc and getFirestore

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {doc, getFirestore} from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,4 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const user = uid => doc(db, `users/${uid}`);
 
