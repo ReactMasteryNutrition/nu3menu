c6ffa7a4a78d23795c1ddc52ce976047a456a5ab
David Belleau <david-belleau@hotmail.com>
2022-08-13 18:28:51 +0200

    feat/add props

--- src/components/myAccount/Modal/DeleteAccount.js
+++ src/components/myAccount/Modal/DeleteAccount.js
@@ -7,6 +7,7 @@ import {
     Input,
     InputGroup,
     InputRightElement,
+    Image,
     FormLabel,
     Modal,
     ModalOverlay,
@@ -79,14 +80,37 @@ const DeleteAccount = () => {
                 Supprimer mon compte
             </Button>
             <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                <ModalOverlay bg="rgba(160, 174, 192, 0.5)" />
+                <ModalOverlay bg={ResponsiveWidth() ? "rgba(160, 174, 192, 0.5)" : "inherit"} />
                 <ModalContent
                     bg="#1A202C"
                     color="#F0FFF4"
-                    width={ResponsiveWidth() ? null : "90%"}
+                    position={ResponsiveWidth() ? null : "fixed"}
+                    minHeight={ResponsiveWidth() ? null : "100vh"}
+                    minWidth={ResponsiveWidth() ? null : "100vw"}
+                    borderRadius={ResponsiveWidth() ? null : 0}
                 >
-                    <ModalHeader>Supprimer mon compte</ModalHeader>
+                    <Box
+                        position={ResponsiveWidth() ? null : "absolute"}
+                        left={ResponsiveWidth() ? null : "50%"}
+                        top={ResponsiveWidth() ? null : "25%"}
+                        width='100%'
+                        transform={ResponsiveWidth() ? null : "translate(-50%, -50%)"}
+                    >
+                        <Image
+                            src="./images/logo_nu3menu.svg" alt="Logo du site"
+                            width={ResponsiveWidth() ? "15rem" : "18rem"}
+                            margin={ResponsiveWidth() ? '1rem auto' : '1rem auto 2rem auto'}
+                        />
+                        <ModalHeader
+                            textAlign="center"
+                            fontSize="1.5rem"
+                            marginBottom="1rem"
+                        >
+                            Supprimer mon compte
+                        </ModalHeader>
+                    </Box>
                     <ModalCloseButton
+                        onClick={onClose}
                         position="absolute"
                         right="0"
                         top="0"
@@ -97,6 +121,7 @@ const DeleteAccount = () => {
                         borderBottomRightRadius={ResponsiveWidth() ? null : 0}
                         borderBottomLeftRadius={ResponsiveWidth() ? 'null' : "1rem"}
                         borderTopLeftRadius={ResponsiveWidth() ? null : 0}
+                        borderTopRightRadius={ResponsiveWidth() ? null : 0}
                         bg="#48bb78"
                         cursor="pointer"
                         _hover={{
@@ -104,21 +129,30 @@ const DeleteAccount = () => {
                         }}
                     />
                     <ModalBody>
-                        <FormLabel>Veuillez-vous authentifier avec votre mot de passe</FormLabel>
-                        <InputGroup size='md' marginBottom="1rem" >
-                            <Input
-                                id='reauthentifyName'
-                                type={passwordVerify ? 'text' : 'password'}
-                                bg='#f0fff4'
-                                color="#1A202C"
-                                ref={addData}
-                            />
-                            <InputRightElement width='4.5rem' >
-                                <Box size='sm' bg='#f0fff4' onClick={handlePassword}>
-                                    {passwordVerify ? <ViewOffIcon color='#48bb78' /> : <ViewIcon color='#48bb78' />}
-                                </Box>
-                            </InputRightElement>
-                        </InputGroup>
+                        <Box
+                            position={ResponsiveWidth() ? null : "absolute"}
+                            left={ResponsiveWidth() ? null : "50%"}
+                            top={ResponsiveWidth() ? null : "50%"}
+                            width={ResponsiveWidth() ? null : '90%'}
+                            transform={ResponsiveWidth() ? null : "translate(-50%, -50%)"}
+                        >
+                            <FormLabel>Veuillez-vous authentifier avec votre mot de passe</FormLabel>
+                            <InputGroup size='md' marginBottom="1rem" >
+                                <Input
+                                    id='reauthentifyName'
+                                    type={passwordVerify ? 'text' : 'password'}
+                                    bg='#f0fff4'
+                                    color="#1A202C"
+                                    padding={ResponsiveWidth() ? null : '1.3rem 0 1.3rem 1rem'}
+                                    ref={addData}
+                                />
+                                <InputRightElement width='4.5rem' padding={ResponsiveWidth() ? null : '1.3rem 0'}>
+                                    <Box size='sm' bg='#f0fff4' onClick={handlePassword}>
+                                        {passwordVerify ? <ViewOffIcon color='#48bb78' /> : <ViewIcon color='#48bb78' />}
+                                    </Box>
+                                </InputRightElement>
+                            </InputGroup>
+                        </Box>
                     </ModalBody>
                     <ModalFooter>
                         <Button
@@ -126,6 +160,12 @@ const DeleteAccount = () => {
                             bg='#48bb78'
                             color="#f0fff4"
                             _hover={{ bgColor: "#a0aec0" }}
+                            padding={ResponsiveWidth() ? null : '1.3rem 0'}
+                            position={ResponsiveWidth() ? null : "absolute"}
+                            left={ResponsiveWidth() ? null : "50%"}
+                            top={ResponsiveWidth() ? null : "70%"}
+                            width={ResponsiveWidth() ? null : "90%"}
+                            transform={ResponsiveWidth() ? null : "translate(-50%, -50%)"}
                         >Valider
                         </Button>
                     </ModalFooter>
