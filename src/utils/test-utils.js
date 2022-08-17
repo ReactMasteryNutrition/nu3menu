82d5cdd9d53851d5c61f77ee9a6ae37412cca028
David Belleau <david-belleau@hotmail.com>
2022-08-17 13:19:56 +0200

    feat/add components

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -1,8 +1,31 @@
 import AuthContextProvider from "../context/authContext"
+import React from "react";
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
 import { render as renderReactTestingLib } from '@testing-library/react'
 
+const theme = extendTheme({
+    styles: {
+        global: {
+            body: {
+                bg: "gray.800",
+                fonts: "Poppins, sans-serif",
+            }
+        }
+    }
+})
+
+const AllProviders = ({ children }) => {
+    return (
+        <ChakraProvider theme={theme}>
+                <AuthContextProvider>
+                    {children}
+                </AuthContextProvider>
+        </ChakraProvider>
+    )
+}
+
 const render = (ui, { ...options } = {}) => {
-    return renderReactTestingLib(ui, { wrapper: AuthContextProvider, ...options })
+    return renderReactTestingLib(ui, { wrapper: AllProviders, ...options })
 }
 
 export * from '@testing-library/react'
