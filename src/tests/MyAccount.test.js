e9de5a87cb4dbcb42d96da9e83167170d4dc3eb6
David Belleau <david-belleau@hotmail.com>
2022-08-21 23:09:33 +0200

    test/delete window.matchMedia and Router

--- src/tests/MyAccount.test.js
+++ src/tests/MyAccount.test.js
@@ -1,42 +1,28 @@
 import { render, screen } from "../utils/test-utils"
 import MyAccount from '../pages/MyAccount'
 import userEvent from '@testing-library/user-event'
-import { BrowserRouter as Router } from "react-router-dom"
-
-beforeAll(() => {
-    window.matchMedia = (query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })
-})
 
 describe('display a modal', () => {
     test('display a modal to modify a name when clicking', () => {
-        render(<Router><MyAccount /></Router>)
+        render(<MyAccount />)
         expect(screen.getByRole('button', { description: "name" })).toBeInTheDocument()
         userEvent.click(screen.getByRole('button', { description: "name" }))
         expect(screen.getByRole('dialog', { name: "Modifier mon prénom" })).toBeInTheDocument()
     })
     test('display a modal to modify an e-mail when clicking', () => {
-        render(<Router><MyAccount /></Router>)
+        render(<MyAccount />)
         expect(screen.getByRole('button', { description: "e-mail" })).toBeInTheDocument()
         userEvent.click(screen.getByRole('button', { description: "e-mail" }))
         expect(screen.getByRole('dialog', { name: "Modifier mon adresse e-mail" })).toBeInTheDocument()
     })
     test('display a modal to modify a password when clicking', () => {
-        render(<Router><MyAccount /></Router>)
+        render(<MyAccount />)
         expect(screen.getByRole('button', { description: "password" })).toBeInTheDocument()
         userEvent.click(screen.getByRole('button', { description: "password" }))
         expect(screen.getByRole('dialog', { name: "Modifier mon mot de passe" })).toBeInTheDocument()
     })
     test('display a modal to delete an user account when clicking', () => {
-        render(<Router><MyAccount /></Router>)
+        render(<MyAccount />)
         expect(screen.getByRole('button', { name: "Supprimer mon compte" })).toBeInTheDocument()
         userEvent.click(screen.getByRole('button', { name: "Supprimer mon compte" }))
         expect(screen.getByRole("dialog", {name:"Supprimer mon compte"})).toBeInTheDocument()
--- src/tests/NavMobile.test.js
+++ src/tests/NavMobile.test.js
@@ -1,24 +1,10 @@
 import { render, screen } from '../utils/test-utils'
 import NavMobile from '../components/navBar/NavMobile'
-import { BrowserRouter as Router } from "react-router-dom"
 import userEvent from '@testing-library/user-event'
-
-beforeAll(() => {
-    window.matchMedia = (query) => ({
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(),
-        removeListener: jest.fn(),
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    })
-})
 
 describe('display a modal', () => {
     test('display a modal to register a visitor when clicking', () => {
-        render(<Router><NavMobile /></Router>)
+        render(<NavMobile />)
         expect(screen.getByLabelText("register")).toBeInTheDocument()
         userEvent.click(screen.getByLabelText("register"))
         expect(global.window.location.href).toContain("/register")
@@ -26,7 +12,7 @@ describe('display a modal', () => {
         expect(screen.getByRole('button', { name: "S'inscrire" })).toBeInTheDocument()
     })
     test('display a modal to login an user when clicking', () => {
-        render(<Router><NavMobile /></Router>)
+        render(<NavMobile />)
         expect(screen.getByLabelText("login")).toBeInTheDocument()
         userEvent.click(screen.getByLabelText("login"))
         expect(global.window.location.href).toContain("/login")
