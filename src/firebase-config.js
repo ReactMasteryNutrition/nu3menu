2d3ca0806b2e7e7772f18957d81049fddb73ba71
David Belleau <david-belleau@hotmail.com>
2022-08-23 18:16:10 +0200

    feat/add getStorage

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {doc, getFirestore} from "firebase/firestore";
-
+import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,7 +15,6 @@ const firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID,
     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
-    
 };
 
 // Initialize Firebase
@@ -23,4 +22,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const user = uid => doc(db, `users/${uid}`);
+export const storage = getStorage(app);
 
