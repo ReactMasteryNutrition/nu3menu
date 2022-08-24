658a9cbf78346d9066c0d849a197d93812a3db35
David Belleau <david-belleau@hotmail.com>
2022-08-24 19:17:48 +0200

    feat/add error boundary and a new route

--- src/userApp.js
+++ src/userApp.js
@@ -11,25 +11,30 @@ import { Box } from '@chakra-ui/react'
 import SearchAndFilter from './components/SearchAndFilter/SearchAndFilter';
 import DayMeal from './components/meal/dayMeal';
 import MenuCreator from './components/menuCreator/menuCreator';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/error/ErrorFallback';
+import Error404 from './pages/Error404'
 
 const UserApp = () => {
     return (
         <Box display='flex' flexDirection='column' minH='100vh'>
             <NavDesktop />
             <NavMobile />
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                {/* <Route path='/user' element={<NutriHomeUser />} /> */}
-                <Route path='/findRecipe' element={<SearchAndFilter />} />
-                <Route path='/menu' element={<DayMeal/>} />
-                <Route path='/myaccount' element={<MyAccount />} />
-                <Route path='/createMenu' element={<MenuCreator />} />
-                {/*<Route path='/menu' element={<UserMenu/>} />*/}
-                {/*<Route path='/menu/:id' element={<MenuDetail/>} />*/}
-                {/*<Route path='/recipe/:id' element={<Recipe/>} />*/}
-            </Routes>
+            <ErrorBoundary FallbackComponent={ErrorFallback}>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/register' element={<Register />} />
+                    <Route path='/login' element={<Login />} />
+                    <Route path='/findRecipe' element={<SearchAndFilter />} />
+                    <Route path='/menu' element={<DayMeal />} />
+                    <Route path='/myaccount' element={<MyAccount />} />
+                    <Route path='/createMenu' element={<MenuCreator />} />
+                    {/*<Route path='/menu' element={<UserMenu/>} />*/}
+                    {/*<Route path='/menu/:id' element={<MenuDetail/>} />*/}
+                    {/*<Route path='/recipe/:id' element={<Recipe/>} />*/}
+                    <Route path='*' element={<Error404/>}/>
+                </Routes>
+            </ErrorBoundary>
             <NutriFooter />
         </Box>
     )
