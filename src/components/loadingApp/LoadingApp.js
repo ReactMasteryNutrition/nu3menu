bf7d66efe845934e7b086f462b4b18180333c1c9
David Belleau <david-belleau@hotmail.com>
2022-08-28 15:20:18 +0200

    feat/create file

---
+++ src/components/loadingApp/LoadingApp.js
@@ -0,0 +1,19 @@
+import { Box, CircularProgress } from "@chakra-ui/react"
+import { ResponsiveWidth } from "../../utils/helper"
+
+const LoadingApp = () => {
+    return (
+        <Box role='alert' w="100%" flexGrow='1' >
+            <CircularProgress
+                isIndeterminate color='#48BB78'
+                size="90%"
+                position="fixed"
+                left="50%"
+                top={ResponsiveWidth() ? "55%" : "30%"}
+                transform="translate(-50%, -50%)"
+            />
+        </Box>
+    )
+}
+
+export default LoadingApp
