c55fa22bb4a3c67aab799021573742018d5cdfb7
David Belleau <david-belleau@hotmail.com>
2022-09-06 17:54:47 +0200

    style/delete comments

--- src/components/SearchAndFilter/Query.js
+++ src/components/SearchAndFilter/Query.js
@@ -2,32 +2,20 @@
 import React from 'react'
 import { Center, Spinner, Text } from '@chakra-ui/react'
 // import test
-// import FetchAxios from '../../utils/FetchAxios'
 import { useEdamam } from '../../utils/helper'
 import CardRecipe from '../Card/CardRecipe';
 //import CardWithGrid from '../Card/CardWithGrid';
 // Function
 export default function Query({ thingSearched, filter, index, category, weekMenu, setWeekMenu }) {
     const [data, setData] = React.useState();
-    // const [error, setError] = React.useState(null);
     const fetchAxios = useEdamam(thingSearched, filter)
-    
+
     React.useEffect(() => {
-        // const controller = new AbortController();
         if (!thingSearched) {
             return
         }
         setData(fetchAxios)
-        /* .then(response => {
-            setData(response)
-        })
-        .catch(error => setError(error))
-        .finally(() => controller.abort()) */
     }, [thingSearched, fetchAxios])
-
-    /* if (error) {
-        throw error
-    } */
     if (!thingSearched) {
         return (
             <Center color='green.50' paddingY='2rem'>Rechercher une recette</Center>
