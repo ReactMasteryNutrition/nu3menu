c577c067083458161ba30e4e32e867ae3bda9c42
David Belleau <david-belleau@hotmail.com>
2022-09-19 21:37:53 +0200

    feat/modify routes

--- src/userApp.js
+++ src/userApp.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box} from '@chakra-ui/react'
+import { Box } from '@chakra-ui/react'
 import { Routes, Route } from 'react-router-dom';
 import { ErrorBoundary } from 'react-error-boundary';
 import LoadingApp from './components/loadingApp/LoadingApp';
@@ -7,11 +7,7 @@ import ErrorFallback from './components/error/ErrorFallback';
 import NavDesktop from './components/navBar/NavDesktop';
 import NavMobile from './components/navBar/NavMobile';
 import NutriFooter from './components/footer/nutriFooter'
-import CreateMenu from './pages/CreateMenu';
-import CurrentMenu from './pages/CurrentMenu';
 import { PrivateRoute } from './utils/helper';
-import Favorite from './pages/Favorite';
-import AllMenus from './pages/AllMenus'
 // pages loaded immediately
 const Home = React.lazy(() => import('./pages/Home'))
 const Register = React.lazy(() => import('./pages/Register'))
@@ -19,10 +15,11 @@ const Login = React.lazy(() => import('./pages/Login'))
 const Error404 = React.lazy(() => import('./pages/Error404'))
 // pages prefetch then loaded after connection
 const MyAccount = React.lazy(() => import(/* webpackPrefetch: true */ './pages/MyAccount'))
-
-// import SearchAndFilter from './components/SearchAndFilter/SearchAndFilter';
-// import DayMeal from './components/meal/dayMeal';
-// import MenuCreator from './components/menuCreator/menuCreator';
+const CurrentMenu = React.lazy(() => import(/* webpackPrefetch: true */ './pages/CurrentMenu'))
+const CreateMenu = React.lazy(() => import(/* webpackPrefetch: true */ './pages/CreateMenu'))
+const Favorite = React.lazy(() => import(/* webpackPrefetch: true */ './pages/Favorite'))
+const AllMenus = React.lazy(() => import(/* webpackPrefetch: true */ './pages/AllMenus'))
+const DetailsMenu = React.lazy(() => import(/* webpackPrefetch: true */ './pages/DetailsMenu'))
 
 const UserApp = () => {
     return (
@@ -32,24 +29,23 @@ const UserApp = () => {
                 <NavMobile />
                 <ErrorBoundary FallbackComponent={ErrorFallback}>
                     <Routes>
-                        <Route path='/' element={<Home/>} />
-                        <Route path='/register' element={<Register/>} />
-                        <Route path='/login' element={<Login/>} />
-                        <Route path='/myaccount' element={<PrivateRoute><MyAccount/></PrivateRoute>} />
-                        {/* <Route path='/findRecipe' element={<SearchAndFilter />} /> */}
-                        <Route path='/menu' element={<PrivateRoute><CurrentMenu/></PrivateRoute>} />
-                        <Route path='/createMenu' element={<PrivateRoute><CreateMenu/></PrivateRoute>} />
-                        <Route path='/favorite' element={<PrivateRoute><Favorite/></PrivateRoute>} />
-                        <Route path='/allmenus' element={<PrivateRoute><AllMenus/></PrivateRoute>} />
-                        {/*<Route path='/menu' element={<UserMenu/>} />*/}
-                        {/*<Route path='/menu/:id' element={<MenuDetail/>} />*/}
-                        {/*<Route path='/recipe/:id' element={<Recipe/>} />*/}
+                        <Route path='/' element={<Home />} />
+                        <Route path='/register' element={<Register />} />
+                        <Route path='/login' element={<Login />} />
+                        <Route element={<PrivateRoute />} >
+                            <Route path='/myaccount' element={<MyAccount />} />
+                            <Route path='/createMenu' element={<CreateMenu />} />
+                            <Route path='/menu' element={<CurrentMenu />} />
+                            <Route path="/:menuId" element={<DetailsMenu />} />
+                            <Route path='/allmenus' element={<AllMenus />} />
+                            <Route path='/favorite' element={<Favorite />} />
+                        </Route>
                         <Route path='*' element={<Error404 />} />
                     </Routes>
                 </ErrorBoundary>
                 <NutriFooter />
             </React.Suspense>
-        </Box>
+        </Box >
     )
 }
 
