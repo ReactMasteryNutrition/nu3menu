9c81bffcc8356d494592dc7168cdb2ccc427d647
David Belleau <david-belleau@hotmail.com>
2022-09-25 14:13:11 +0200

    feat/modify links

--- src/components/data/dataLink.js
+++ src/components/data/dataLink.js
@@ -2,13 +2,13 @@ import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa'
 
 export const  socialIconLink = [
     { id: "fa", label: <FaFacebook />, to: "https://www.facebook.com", colori: "green.400" , aLabel :"Facebook"},
-    { id: "tw", label: <FaTwitter />, to: "https://www.facebook.com", colori: "green.400" , aLabel :"twitter"},
-    { id: "in", label: <FaInstagram />, to: "https://www.facebook.com", colori: "green.400", aLabel :"Instagram" }
+    { id: "tw", label: <FaTwitter />, to: "https://www.twitter.com", colori: "green.400" , aLabel :"twitter"},
+    { id: "in", label: <FaInstagram />, to: "https://www.instagram.com", colori: "green.400", aLabel :"Instagram" }
 ]
 
 export const linkListFooter = [
-    { id: "PP", url: "https://dietplannerapp.com/privacy-policy/", label: "Privacy" },
-    { id: "T", url: "https://dietplannerapp.com/terms/", label: "Terms" },
-    { id: "C", url: "", label: "Contact" }
+    { id: "PP", url: "/", label: "Privacy" },
+    { id: "T", url: "/", label: "Terms" },
+    { id: "C", url: "/", label: "Contact" }
 
 ]
