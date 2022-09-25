1a1424a299e0c811dbc12e6206b1206310f0de20
Mathieu <mathieu.herbelet@hotmail.fr>
2022-09-25 09:56:28 +0200

    Début ajout des states et suppression des blocs commentés

--- src/components/contact/FormContact.js
+++ src/components/contact/FormContact.js
@@ -1,133 +1,173 @@
-import {Box, Button, Center, Flex, FormControl, FormLabel, Heading, HStack, Input, Textarea} from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Flex,
+  FormControl,
+  FormLabel,
+  Heading,
+  HStack,
+  Input,
+  Textarea,
+} from "@chakra-ui/react";
 import React from "react";
-import {ResponsiveWidth} from "../../utils/helper";
+import { ResponsiveWidth } from "../../utils/helper";
+import {useForm} from "react-hook-form";
 
 const FormContact = () => {
-    return (
-        <>
-            <Flex
-                flexDirection="column"
-                justifyContent="center"
-                alignItems="center"
-                gap="3rem"
-                margin="5rem 0 8rem 0"
-            >
-                <Center mb='2rem'>
-                    <Heading as='h1' size='xl' color={"green.50"} >Contact us</Heading>
-                </Center>
-                <Center>
-                <Flex
-                    flexDirection="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    width={ResponsiveWidth() ? null : "80%"}
-                >
-                    <FormControl display="flex" flexDir="column" gap="4">
-                        <HStack spacing="6">
-                            <Box w="100%">
-                                <FormControl id="first-name" isRequired>
-                                    <FormLabel color={"green.50"}>First-name</FormLabel>
-                                    <Input
-                                        type="text"
-                                        color={"green.50"}
-                                        borderColor='#48bb78'
-                                        _focusVisible={{borderColor: 'green.50'}}
-                                    />
-                                </FormControl>
-                            </Box>
-                            <Box w="100%">
-                                <FormControl id="last-name" isRequired>
-                                    <FormLabel color={"green.50"}>Last-name</FormLabel>
-                                    <Input
-                                        type="text"
-                                        color={"green.50"}
-                                        borderColor='#48bb78'
-                                        _focusVisible={{borderColor: 'green.50'}}
-                                    />
-                                </FormControl>
-                            </Box>
-                        </HStack>
-                        <HStack spacing="4">
-                            <Box w="100%">
-                                <FormControl id="email" isRequired>
-                                    <FormLabel color={"green.50"}>Email</FormLabel>
-                                    <Input
-                                        type="email"
-                                        color={"green.50"}
-                                        borderColor='#48bb78'
-                                        _focusVisible={{borderColor: 'green.50'}}
-                                    />
-                                </FormControl>
-                            </Box>
-                        </HStack>
-                        <HStack spacing="4">
-                            <Box w="100%">
-                                <FormControl id="object" isRequired>
-                                    <FormLabel color={"green.50"}>Object</FormLabel>
-                                    <Input
-                                        type="text"
-                                        color={"green.50"}
-                                        borderColor='#48bb78'
-                                        _focusVisible={{borderColor: 'green.50'}}
-                                    />
-                                </FormControl>
-                            </Box>
-                        </HStack>
-                        <HStack spacing="4">
-                            <Box w="100%">
-                                <FormControl id="message" isRequired>
-                                    <FormLabel color={"green.50"}>Message</FormLabel>
-                                    <Textarea
-                                        color={"green.50"}
-                                        borderColor='#48bb78'
-                                        _focusVisible={{borderColor: 'green.50'}}
-                                    />
-                                </FormControl>
-                            </Box>
-                        </HStack>
-                        <HStack spacing="4" justify="center">
-                                <Button
-                                    colorScheme="green"
-                                    variant="solid"
-                                    width="10rem"
-                                    type="submit"
-                                    bg="#48bb78"
-                                    color={"green.50"}
-                                    fontWeight="bold"
-                                    fontSize="lg"
-                                    mt={4}
-                                    _hover={{ bg: "#9faec0" }}
-                                >
-                                    Send
-                                </Button>
-                        </HStack>
+  const { handleSubmit, errors, register, formState } = useForm();
+  const validateEmail = (value) => {
+    let error;
+    if (!value) {
+      error = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(value)) {
+      error = "Email address is invalid";
+    }
+    return error || true;
+  }
+  const validateName = (value) => {
+    let error;
+    if (!value) {
+      error = "Name is required";
+    }
+    return error || true;
+  }
+
+  const onSubmit = (values) => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        alert(JSON.stringify(values, null, 2));
+        resolve();
+      }, 3000);
+    });
+  }
+
+
+  return (
+    <>
+      <Flex
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="3rem"
+        margin="5rem 0 8rem 0"
+      >
+        <Center mb="2rem">
+          <Heading as="h1" size="xl" color={"green.50"}>
+            Contact us
+          </Heading>
+        </Center>
+        <Center>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            width={ResponsiveWidth() ? null : "80%"}
+          >
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <FormControl display="flex" flexDir="column" gap="4">
+                <HStack spacing="6">
+                  <Box w="100%">
+                    <FormControl id="first-name" isInvalid={errors.name} isRequired>
+                      <FormLabel color={"green.50"}>First-name</FormLabel>
+                      <Input
+                        type="text"
+                        color={"green.50"}
+                        borderColor="#48bb78"
+                        _focusVisible={{ borderColor: "green.50" }}
+                        ref={register({
+                          pattern: {
+                            value: /[A-Za-z]/,
+                            message:
+                                "Please enter a valid first name",
+                          }
+                        })}
+                      />
                     </FormControl>
-                </Flex>
-                </Center>
+                  </Box>
+                  <Box w="100%">
+                    <FormControl id="last-name" isRequired>
+                      <FormLabel color={"green.50"}>Last-name</FormLabel>
+                      <Input
+                        type="text"
+                        color={"green.50"}
+                        borderColor="#48bb78"
+                        _focusVisible={{ borderColor: "green.50" }}
+                        ref={register({
+                          pattern: {
+                            value: /[A-Za-z]/,
+                            message:
+                                "Last name must contain only letters",
+                          }
+                        })}
+                      />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <HStack spacing="4">
+                  <Box w="100%">
+                    <FormControl id="email" isRequired>
+                      <FormLabel color={"green.50"}>Email</FormLabel>
+                      <Input
+                        type="email"
+                        color={"green.50"}
+                        borderColor="#48bb78"
+                        _focusVisible={{ borderColor: "green.50" }}
+                        ref={register({
+                          pattern: /^\S+@\S+$/
+                        })}
+                      />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <HStack spacing="4">
+                  <Box w="100%">
+                    <FormControl id="object" isRequired>
+                      <FormLabel color={"green.50"}>Object</FormLabel>
+                      <Input
+                        type="text"
+                        color={"green.50"}
+                        borderColor="#48bb78"
+                        _focusVisible={{ borderColor: "green.50" }}
+                      />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <HStack spacing="4">
+                  <Box w="100%">
+                    <FormControl id="message" isRequired>
+                      <FormLabel color={"green.50"}>Message</FormLabel>
+                      <Textarea
+                        color={"green.50"}
+                        borderColor="#48bb78"
+                        _focusVisible={{ borderColor: "green.50" }}
+                      />
+                    </FormControl>
+                  </Box>
+                </HStack>
+                <HStack spacing="4" justify="center">
+                  <Button
+                    colorScheme="green"
+                    variant="solid"
+                    width="10rem"
+                    type="submit"
+                    bg="#48bb78"
+                    color={"green.50"}
+                    fontWeight="bold"
+                    fontSize="lg"
+                    mt={4}
+                    _hover={{ bg: "#9faec0" }}
+                  >
+                    Send
+                  </Button>
+                </HStack>
+              </FormControl>
+            </form>
+          </Flex>
+        </Center>
+      </Flex>
+    </>
+  );
+};
 
-
-
-                {/*<Box w="100%">
-                                <FormLabel color={"green.50"} htmlFor="name">Nom</FormLabel>
-                                <Input id="name" color="white" borderColor='#48bb78'_hover={{borderColor: "green.50"}} type="text"/>
-                            </Box>
-                            <Box w="100%">
-                                <FormLabel color={"green.50"} htmlFor="username">Prénom</FormLabel>
-                                <Input id="email" type="email" color="white" borderColor='#48bb78'_hover={{borderColor: "green.50"}} />
-                            </Box>
-                            */}
-
-                {/*<form>
-                    <input type="text" placeholder="Nom" />
-                    <input type="text" placeholder="Prénom" />
-                    <input type="email" placeholder="Email" />
-                    <input type="text" placeholder="Objet" />
-                    <textarea placeholder="Message" />
-                    <button type="submit">Envoyer</button>
-                </form> */}
-            </Flex>
-        </>
-    )
-}
-
-export default FormContact
+export default FormContact;
