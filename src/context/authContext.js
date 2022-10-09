71f12044521fdfaa9ec664d5d888fcf300cbcf83
David Belleau <david-belleau@hotmail.com>
2022-10-09 12:53:53 +0200

    feat/delete a condition + modify functions

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,11 +1,9 @@
-import {createContext, useCallback, useMemo, useState, useEffect, useContext} from 'react'
-import {createUserWithEmailAndPassword, onAuthStateChanged, signInWithPopup, signInWithEmailAndPassword, signOut, GoogleAuthProvider, sendPasswordResetEmail, sendEmailVerification} from 'firebase/auth'
-import {auth, db} from '../firebase-config';
-import {setDoc, doc, getDoc, serverTimestamp} from "firebase/firestore";
-import LoadingApp from '../components/loadingApp/LoadingApp';
-import { useLoading } from '../utils/helper';
+import { createContext, useCallback, useMemo, useState, useEffect, useContext } from 'react'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithPopup, signInWithEmailAndPassword, signOut, GoogleAuthProvider, sendPasswordResetEmail, sendEmailVerification } from 'firebase/auth'
+import { auth, db } from '../firebase-config';
+import { setDoc, doc, getDoc, serverTimestamp } from "firebase/firestore";
 
-export const AuthContext = createContext( null );
+export const AuthContext = createContext(null);
 
 export const useAuth = () => {
     const context = useContext(AuthContext)
@@ -16,22 +14,18 @@ export const useAuth = () => {
 }
 
 export default function AuthContextProvider(props) {
-    const [authError, setAuthError] = useState()
     const [currentUser, setCurrentUser] = useState(null)
-    const { status } = useLoading()
     const register = useCallback((email, password) => {
         return createUserWithEmailAndPassword(auth, email, password)
-            .catch(err => setAuthError(err))
-    },[])
+    }, [])
 
     const login = useCallback((email, password) => {
         return signInWithEmailAndPassword(auth, email, password)
-            .catch(err => setAuthError(err))
     }, []);
 
     const logout = useCallback(() => {
         return signOut(auth)
-    },[]);
+    }, []);
 
     const resetPassword = useCallback((email) => {
         return sendPasswordResetEmail(auth, email)
@@ -39,7 +33,7 @@ export default function AuthContextProvider(props) {
 
     const verifyEmail = useCallback(() => {
         return sendEmailVerification(auth.currentUser)
-    },[]);
+    }, []);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
@@ -52,14 +46,13 @@ export default function AuthContextProvider(props) {
         return () => {
             unsubscribe() // on désinscrit l'écouteur de l'authentification
         }
-    } , []) // on ne déclenche pas l'écouteur de l'authentification si on ne change pas le currentUser
+    }, []) // on ne déclenche pas l'écouteur de l'authentification si on ne change pas le currentUser
 
     const signInWithGoogle = useCallback(() => {
         const provider = new GoogleAuthProvider()
-        return signInWithPopup(auth,provider)
-    },[]);
+        return signInWithPopup(auth, provider)
+    }, []);
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
     const newCreateUserInFirestoreDatabase = async (UserCredential) => {
         const userRef = doc(db, `users/${UserCredential.user.uid}`);
         const userData = await getDoc(userRef);
@@ -76,11 +69,8 @@ export default function AuthContextProvider(props) {
         }
     }
 
-
     const value = useMemo(() => ({
         currentUser,
-        status,
-        authError,
         register,
         login,
         logout,
@@ -88,12 +78,8 @@ export default function AuthContextProvider(props) {
         resetPassword,
         newCreateUserInFirestoreDatabase,
         verifyEmail,
-    }),[currentUser, status, authError, register, login, logout, signInWithGoogle, resetPassword, verifyEmail])
+    }), [currentUser, register, login, logout, signInWithGoogle, resetPassword, verifyEmail])
 
-    // display a loading component waiting for the connection
-    if (status === 'fetching') {
-        return <LoadingApp />
-    }
     return (
         <AuthContext.Provider value={value}>
             {/* !loading && props.children} */}
