455e3951c05610679d5a7ae64bb34004c76220c7
David Belleau <david-belleau@hotmail.com>
2022-10-09 12:50:41 +0200

    feat/add a ternary

--- src/components/loadingApp/LoadingApp.js
+++ src/components/loadingApp/LoadingApp.js
@@ -1,18 +1,22 @@
 import { Box, CircularProgress } from "@chakra-ui/react"
+import { useAuth } from "../../context/authContext"
 import { ResponsiveWidth } from "../../utils/helper"
 
 const LoadingApp = () => {
+    const { currentUser } = useAuth()
     return (
         <Box role='alert' w="100%" flexGrow='1' >
-            <CircularProgress
-                isIndeterminate
-                color='#48BB78'
-                size="90%"
-                position="fixed"
-                left="50%"
-                top={ResponsiveWidth() ? "55%" : "30%"}
-                transform="translate(-50%, -50%)"
-            />
+            {!currentUser ?
+                <CircularProgress
+                    isIndeterminate
+                    color='#48BB78'
+                    size="90%"
+                    position="fixed"
+                    left="50%"
+                    top={ResponsiveWidth() ? "55%" : "30%"}
+                    transform="translate(-50%, -50%)"
+                />
+                : null}
         </Box>
     )
 }
